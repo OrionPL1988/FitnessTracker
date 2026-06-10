a37cb504214b1f3e372f952bd5d08ede70d09ca9
krzys <krzysztofrusak88@gmail.com>
2026-06-10 13:57:41 +0200

    lab04

--- src/main/java/pl/wsb/fitnesstracker/user/api/UserEmailDto.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserEmailDto.java
@@ -2,5 +2,8 @@ package pl.wsb.fitnesstracker.user.api;
 
 import jakarta.annotation.Nullable;
 
-public record UserEmailDto(@Nullable Long id, String email) {
+public record UserEmailDto(
+        @Nullable Long id,
+        String email
+) {
 }
--- src/main/java/pl/wsb/fitnesstracker/user/api/UserSimpleDto.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserSimpleDto.java
@@ -2,5 +2,9 @@ package pl.wsb.fitnesstracker.user.api;
 
 import jakarta.annotation.Nullable;
 
-public record UserSimpleDto(@Nullable Long id, String firstName, String lastName) {
+public record UserSimpleDto(
+        @Nullable Long id,
+        String firstName,
+        String lastName
+) {
 }
